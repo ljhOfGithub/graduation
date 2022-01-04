54253f04b8f4913591df4fae397ca4efc4e9ac85
ljhOfGithub <42089575+ljhOfGithub@users.noreply.github.com>
2022-01-04 21:45:16 +0800

    update

--- crawler.py
+++ crawler.py
@@ -4,29 +4,40 @@ import requests
 from ast import literal_eval
 import csv
 import xlrd
-apikey = "ZF9TQA39PFPPUD7VCDK2Q9ZVD2M72N2HGZ"
+import yaml
+# apikey = "ZF9TQA39PFPPUD7VCDK2Q9ZVD2M72N2HGZ"
+apikey = "P3FE926UGARGQF8HKPM4XWJ38CJAGX5WHZ"
+
 def deduplicate():
     with open('bitpoint.txt','r') as f:
         list1 = literal_eval(f.read())
+        list1 = [addr.lower() for addr in list1]
     with open('cryptopia.txt','r') as f:
         list2 = literal_eval(f.read())
+        list2 = [addr.lower() for addr in list2]
     with open('etherdelta.txt','r') as f:
         list3 = literal_eval(f.read())
+        list3 = [addr.lower() for addr in list3]
     with open('LendfMe.txt','r') as f:
         list4 = literal_eval(f.read())
+        list4 = [addr.lower() for addr in list4]
     with open('phishaddr.txt','r') as f:
         list5 = literal_eval(f.read())
+        list5 = [addr.lower() for addr in list5]
     with open('upbit.txt','r') as f:
         list6 = literal_eval(f.read())
+        list6 = [addr.lower() for addr in list6]
     with open('scam.txt','r') as f:
         list7 = literal_eval(f.read())
+        list7 = [addr.lower() for addr in list7]
     with open('db.txt','r') as f:
         list8 = literal_eval(f.read())
+        list8 = [addr.lower() for addr in list8]
     print(type(list1))
     alladdr = list(set(list1 + list2 + list3 + list4 + list5 + list6 + list7 + list8))
     print(len(alladdr))
-    with open('addr.txt','w',encoding='utf-8') as f:
-        print(alladdr,file=f)
+    # with open('addr.txt','w',encoding='utf-8') as f:
+    #     print(alladdr,file=f)
 
 def readxlsx():
     workbook = xlrd.open_workbook('scam.xlsx')
@@ -41,23 +52,117 @@ def readxlsx():
                         addrlist.append(addr)
     with open('scam.txt','w',encoding='utf-8') as f:
         print(addrlist,file=f)
-def getNtxs():
+
+def writedb():
+    with open('urls.yaml','r',encoding='utf-8') as f:
+        with open('dblist.txt','w',encoding='utf-8')as f2:
+            with open('temp.txt','w',encoding='utf-8') as f3:
+                temp = yaml.load(f.read(),Loader=yaml.FullLoader)
+                print(temp,file=f2)
+                for item in temp:
+                    f3.write(str(item) + '\n')
+    with open('dblist.txt','r',encoding='utf-8') as f:
+        itemlist = literal_eval(f.read())
+        print(type(itemlist))
+def readdb():
+    with open('dblist.txt','r',encoding='utf-8') as f:
+        itemlist = literal_eval(f.read())
+        addrlist = []
+        for item in itemlist:
+            if 'addresses' in item.keys() and 'ETH' in item['addresses']:
+                    addrs = item['addresses']['ETH']
+                    for addr in addrs:
+                        if len(addr) == 42:
+                            addrlist.append(addr)
+    addrlist = [addr.lower() for addr in addrlist]
+    addrlist = list(set(addrlist))
+    print(len(addrlist))
+    with open('db.txt','w',encoding='utf-8') as f:
+        print(addrlist,file=f)
+
+def getNtxs1():
     with open('addr.txt','r',encoding='utf-8') as f:
         addrlist = literal_eval(f.read())
-    addrlist = addrlist[0:2000]
+    addrlist = addrlist[0:1000]
     session = requests.Session()
-    with open('ntx1.csv','w',encoding='utf-8') as f:
+    with open('ntx1.csv','w',encoding='utf-8',newline='') as f:
         writer = csv.writer(f)
         writer.writerow(["blockNumber","timeStamp","hash","nonce","blockHash","transactionIndex",
                          "from","to","value","gas","gasPrice","isError","txreceipt_status","input",
                          "contractAddress","cumulativeGasUsed","gasUsed","confirmations"])
+        number = 0
         for addr in addrlist:
             url = "https://api.etherscan.io/api?module=account&action=txlist&address="+addr+"&startblock=0&endblock=99999999&page=1&offset=10000&sort=asc&apikey=" + apikey
-            result = literal_eval(session.get(url).text)['result']
-            if len(result) and result != "Error!":
+            results = literal_eval(session.get(url).text)['result']
+            number += 1
+            print(number)
+            try:
+                if len(results):
+                    for result in results:
+                        writer.writerow([result['blockNumber'],result['timeStamp'],result['hash'],result['nonce'],result['blockHash'],
+                                        result['transactionIndex'],result['from'],result['to'],result['value'],result['gas'],result['gasPrice'],
+                                        result['isError'],result['txreceipt_status'],result['input'],result['contractAddress'],result['cumulativeGasUsed'],
+                                        result['gasUsed'],result['confirmations']])
+            except Exception:
+                print(Exception)
+                print(url)
                 print(result)
-                print(type(result))
-
+                print(addr)
+def getNtxs2():
+    with open('addr.txt','r',encoding='utf-8') as f:
+        addrlist = literal_eval(f.read())
+    addrlist = addrlist[1000:3000]
+    session = requests.Session()
+    with open('ntx2.csv','w',encoding='utf-8',newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(["blockNumber","timeStamp","hash","nonce","blockHash","transactionIndex",
+                         "from","to","value","gas","gasPrice","isError","txreceipt_status","input",
+                         "contractAddress","cumulativeGasUsed","gasUsed","confirmations"])
+        number = 0
+        for addr in addrlist:
+            url = "https://api.etherscan.io/api?module=account&action=txlist&address="+addr+"&startblock=0&endblock=99999999&page=1&offset=10000&sort=asc&apikey=" + apikey
+            results = literal_eval(session.get(url).text)['result']
+            number += 1
+            print(number)
+            try:
+                if len(results):
+                    for result in results:
+                        writer.writerow([result['blockNumber'],result['timeStamp'],result['hash'],result['nonce'],result['blockHash'],
+                                        result['transactionIndex'],result['from'],result['to'],result['value'],result['gas'],result['gasPrice'],
+                                        result['isError'],result['txreceipt_status'],result['input'],result['contractAddress'],result['cumulativeGasUsed'],
+                                        result['gasUsed'],result['confirmations']])
+            except Exception:
+                print(Exception)
+                print(url)
+                print(result)
+                print(addr)
+def getNtxs3():
+    with open('addr.txt','r',encoding='utf-8') as f:
+        addrlist = literal_eval(f.read())
+    addrlist = addrlist[2392:4000]
+    session = requests.Session()
+    with open('ntx3.csv','w',encoding='utf-8',newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(["blockNumber","timeStamp","hash","nonce","blockHash","transactionIndex",
+                         "from","to","value","gas","gasPrice","isError","txreceipt_status","input",
+                         "contractAddress","cumulativeGasUsed","gasUsed","confirmations"])
+        number = 0
+        for addr in addrlist:
+            url = "https://api.etherscan.io/api?module=account&action=txlist&address="+addr+"&startblock=0&endblock=99999999&page=1&offset=10000&sort=asc&apikey=" + apikey
+            results = literal_eval(session.get(url).text)['result']
+            number += 1
+            print(number)
+            try:
+                if len(results):
+                    for result in results:
+                        writer.writerow([result['blockNumber'],result['timeStamp'],result['hash'],result['nonce'],result['blockHash'],
+                                        result['transactionIndex'],result['from'],result['to'],result['value'],result['gas'],result['gasPrice'],
+                                        result['isError'],result['txreceipt_status'],result['input'],result['contractAddress'],result['cumulativeGasUsed'],
+                                        result['gasUsed'],result['confirmations']])
+            except Exception:
+                print(Exception)
+                print(url)
+                print(result)
+                print(addr)
 if __name__ == '__main__':
-    deduplicate()
-    # readxlsx()
+    getNtxs3()
