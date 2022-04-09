0172703ce55e1421b9380aaf8508dfc658cc30d1
ljhOfGithub <42089575+ljhOfGithub@users.noreply.github.com>
2022-04-09 15:53:52 +0800

    update

--- twint.py
+++ twint.py
@@ -1,10 +1,28 @@
+from json.tool import main
 import twint
 #configuration
-c = twint.Config()
-c.Search = "0x0059b14e35daB1b4EEe1e2926C7A5660dA66F747"
-c.Store_csv = True
-c.Proxy_host = '10.112.235.161'
-c.Proxy_port = 1080
-c.Proxy_type = "http"
-# Run
-twint.run.Search(c)
+from ast import literal_eval
+def mytwint():
+    # with open('addr.txt','r',encoding='utf-8') as f:
+    #     addrlist = literal_eval(f.read())
+    addrlist = ['0x0059b14e35daB1b4EEe1e2926C7A5660dA66F747']
+    try:
+        for addr in addrlist:
+            c = twint.Config()
+            c.Search = addr
+            print(type(addr))
+            c.Store_csv = True
+            # c.Proxy_host = '47.88.50.108'
+            # c.Proxy_port = 1080
+            # c.Proxy_type = "http"
+            # c.Count = True
+            c.Lang = "en"
+            # c.Min_wait_time = 10
+            c.Retries_count = 4
+            c.Output = 't.csv'
+            # Run
+            twint.run.Search(c)
+    except Exception:
+        pass
+if __name__ == '__main__':
+    mytwint()
